dd0de196c47a7def959fce27c972648c4e05e68b
Rohan Mishra <rohanmishra2017@gmail.com>
2017-09-23 09:01:01 -0400

    added expres

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
-var Clarifai = require('clarifai');
+var http = require('http');
+var url = require('url');
+var querystring = require('querystring');
+var express = require('express');
+var clarifai = require('clarifai');
 
-var app = new Clarifai.App({
+// Set up server
+var port = process.env.PORT || 8080;
+
+var clarifai = new Clarifai.App({
     apiKey: 'edac3a3bf1654cc29a7ecf97f921fbf9'
 });
 
-// predict the contents of an image by passing in a url
-app.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/metro-north.jpg').then(
-    function(response) {
-      console.log(response);
-    },
-    function(err) {
-      console.error(err);
-    }
-);
+var app = express();
+
+// Heartbeat
+app.get('/', function(req, res) {
+    res.send(200)
+});
+
+// POST request w/ image and return first five objects from Clarifai API
+app.get('/image', function(req, res) {
+    var imageURL = req.param('data');
+});
+
+// Take 
+
+// Start server
+app.listen(port);
+console.log('Express server listening on port ' + app.get('port'));
